69ab8da2d208689d847b5f2cdd886c43c2d0c163
Curley Smith <curleysmithsau@gmail.com>
2018-09-13 01:38:38 -0500

    Build the Basket/Cart

---
+++ MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+    public class Basket: BaseEntity
+    {
+        //Setting this a virtual item says that the it automatically load all items
+        //from the database to the collection. Otherwise known as Lazy Loading
+        public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+        public Basket()
+        {
+            this.BasketItems = new List<BasketItem>();
+        }
+    }
+}
